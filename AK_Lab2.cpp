edad63e82ac506a0bd931c24e8fc5c056367ec31
VadimIliash <71008901+VadimIliash@users.noreply.github.com>
2021-10-29 23:28:52 +0300

    Add files via upload

---
+++ AK_Lab2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Function.cpp"
+
+int main() {
+    print_lab_info();
+    return 0;
+}
---
+++ Function.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void print_lab_info() {
+    std::cout << "It is my 2 lab\n";
+}
